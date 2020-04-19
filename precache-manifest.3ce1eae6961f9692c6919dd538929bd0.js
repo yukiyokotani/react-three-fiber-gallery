7c85a303a431c7679952105617d6363d8dc4bbfa
Yuki Yokotani <yokotani.yuki@gmail.com>
2020-04-20 00:42:33 +0900

    Updates

---
+++ precache-manifest.3ce1eae6961f9692c6919dd538929bd0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f34310a86064aa73cb90d5edaef1118",
+    "url": "/react-three-fiber-starter/index.html"
+  },
+  {
+    "revision": "6994ea327669071c9230",
+    "url": "/react-three-fiber-starter/static/js/2.d3696dda.chunk.js"
+  },
+  {
+    "revision": "c9b31f27ce0803bcbda04bfd0eba249e",
+    "url": "/react-three-fiber-starter/static/js/2.d3696dda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e7ed87a9b2629bfd65f",
+    "url": "/react-three-fiber-starter/static/js/main.4ffe5e4f.chunk.js"
+  },
+  {
+    "revision": "87c3d3505a528b654e0c",
+    "url": "/react-three-fiber-starter/static/js/runtime-main.c4cfdab4.js"
+  }
+]);
